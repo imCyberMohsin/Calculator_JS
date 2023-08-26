c7ecafb55f64b2cd4b2841de8e4f33d6ed663a5c
imCyberMohsin <mohsinansari7033@gmail.com>
2023-08-26 11:01:48 +0530

    updated

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //* Calculator Script 
 
+let str = "";
+
+const buttons = document.querySelectorAll('.btn');
+
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            str = eval(str);
+            document.querySelector('.screen').value = str;
+        } else if (e.target.innerHTML == 'AC') {
+            str = "";
+            document.querySelector('.screen').value = str;
+        } else {
+            // console.log(e.target);
+            str = str + e.target.innerHTML;
+            document.querySelector('.screen').value = str;
+        }
+    })
+})
