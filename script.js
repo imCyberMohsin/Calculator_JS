1b4395b10100e060be158f76d55873377b8ccf7d
imCyberMohsin <mohsinansari7033@gmail.com>
2023-08-26 11:45:38 +0530

    * fixed

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ const buttons = document.querySelectorAll('.btn');
 
 Array.from(buttons).forEach((buttons) => {
     buttons.addEventListener('click', (e) => {
+        // = btn
         if (e.target.innerHTML == '=') {
             str = eval(str);
             document.querySelector('.screen').value = str;
+        // AC btn
         } else if (e.target.innerHTML == 'AC') {
             str = "";
             document.querySelector('.screen').value = str;
-        } else {
+        // DE btn
+        } else if (e.target.innerHTML == 'DE') {
+            str = str.slice(0, str.length - 1);
+            document.querySelector('.screen').value = str;
+        // other btn
+        }
+        else {
             // console.log(e.target);
             str = str + e.target.innerHTML;
             document.querySelector('.screen').value = str;
